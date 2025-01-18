3221576dd10ce910a4e8e23ef498b330333d6d52
Laik_Sheremetyev <ya@laik14.ru>
2025-01-18 20:44:27 +0300

    deploy

--- script.js
+++ script.js
@@ -40,27 +40,55 @@ window.addEventListener('resize', function () {
 // Инициализация при загрузке страницы
 window.dispatchEvent(new Event('resize'));
 
-ymaps.ready(init);
-
-function init() {
-    // Инициализация карты
+ymaps.ready(function () {
     var myMap = new ymaps.Map("yandex-map", {
-        center: [53.256792, 34.313202], // Координаты вашего места
-        zoom: 16, // Масштаб
-        controls: ['zoomControl', 'fullscreenControl'] // Элементы управления
+        center: [53.256792, 34.313202],
+        zoom: 16
     });
 
     // Создаем метку с названием организации
     var myPlacemark = new ymaps.Placemark([53.256792, 34.313202], {
-        hintContent: 'Шанс Есть' // Текст, который появляется при наведении
+        hintContent: 'Шанс Есть',
+        balloonContent: '<div class="myCustomBalloon">Центр "Шанс Есть"</div>' // Добавляем кастомный контент
     });
 
     // Добавляем метку на карту
     myMap.geoObjects.add(myPlacemark);
 
-    // Открываем балун с названием организации при загрузке карты
+    // Открываем балун при загрузке карты
     myPlacemark.balloon.open();
-}
+
+    // Обработчик для коррекции позиции балуна на мобильных устройствах
+    myPlacemark.balloon.events.add('open', function () {
+        if (window.innerWidth <= 768) {
+            // Корректируем позицию балуна на мобильных устройствах, если необходимо
+            myPlacemark.balloon.setPosition([53.256792, 34.313202]);
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var callButton = document.getElementById('call-button'); // Получаем элемент кнопки звонка
+    var footer = document.querySelector('footer'); // Получаем футер
+
+    window.addEventListener('scroll', function () {
+        var footerRect = footer.getBoundingClientRect(); // Получаем положение футера
+        var windowHeight = window.innerHeight; // Высота окна браузера
+
+        // Если футер достигает нижней части экрана
+        if (footerRect.top <= windowHeight) {
+            // Останавливаем кнопку на уровне футера
+            callButton.style.bottom = (footerRect.height + 20) + 'px'; // Размещаем кнопку на 20px выше футера
+        } else {
+            // Если футер не виден, фиксируем кнопку внизу страницы
+            callButton.style.bottom = '20px'; // Возвращаем кнопку к фиксированному положению
+        }
+    });
+});
+
+
+
+
 
 
 
